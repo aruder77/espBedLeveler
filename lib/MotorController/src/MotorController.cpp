9f86f2f71ec728161e48ff0b587a735af541ccdc
Axel Ruder <axel.ruder@gmail.com>
2019-12-22 09:02:56 +0100

    test cycle initial version

--- lib/MotorController/src/MotorController.cpp
+++ lib/MotorController/src/MotorController.cpp
@@ -5,9 +5,58 @@ const char *MotorController::getName() {
 }
 
 void MotorController::setup() {
+    // initialize pwm output pin
+	pinMode(pwmPin, OUTPUT);
 
+    ledcSetup(pwmChannel, 4000, 8);
+    ledcAttachPin(pwmPin, pwmChannel);
+
+    // initialize current pin 
+    //Characterize ADC at particular atten
+    adc_chars = (esp_adc_cal_characteristics_t *)calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_0db, ADC_WIDTH_BIT_12, 3300, adc_chars);
+
+    adc1_config_width(ADC_WIDTH_12Bit);
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_0db);
+
+    Log.notice("coeff_a: %d\n", adc_chars->coeff_a);
+    Log.notice("coeff_b: %d\n", adc_chars->coeff_b);
+    Log.notice("v_ref: %d\n", adc_chars->vref);
+    //Check type of calibration value used to characterize ADC
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        Log.notice("eFuse Vref\n");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        Log.notice("Two Point\n");
+    } else {
+        Log.notice("Default\n");
+    }    
 }
 
 void MotorController::everySecond() {
-    
+    // read current from pin 32
+    uint32_t currentVoltage = readVoltage(ADC1_CHANNEL_4);
+    Log.notice("current voltage: %d\n", currentVoltage);
+
+    ledcWrite(pwmChannel, value);
+    delay(200);
+
+    if (rising) {
+        if (value == 255) {
+            delay(5000);
+            rising = false;
+        } else if (value == 0) {
+            rising = true;
+        } else {
+            if (rising) {
+                value++;
+            } else {
+                value--;
+            }
+        }
+    }
+}
+
+uint32_t MotorController::readVoltage(adc1_channel_t channel) {
+    int adc = adc1_get_raw(channel);
+    return esp_adc_cal_raw_to_voltage(adc, adc_chars);
 }
--- lib/MotorController/src/MotorController.h
+++ lib/MotorController/src/MotorController.h
@@ -2,6 +2,10 @@
 #define MOTORCONTROLLER_H_
 
 #include <Module.h>
+#include <driver/adc.h>
+#include <esp_adc_cal.h>
+#include <ArduinoLog.h>
+
 
 class MotorController : public Module {
     public:
@@ -10,6 +14,16 @@ class MotorController : public Module {
         virtual void everySecond();
 
     private:
+        uint8_t pwmPin = 27;
+        uint8_t pwmChannel = 1;
+
+        uint8_t currentPin = 32;
+        esp_adc_cal_characteristics_t *adc_chars;
+
+        uint8_t value = 0;
+        bool rising = true;
+
+        uint32_t readVoltage(adc1_channel_t channel);
 
 };
 
